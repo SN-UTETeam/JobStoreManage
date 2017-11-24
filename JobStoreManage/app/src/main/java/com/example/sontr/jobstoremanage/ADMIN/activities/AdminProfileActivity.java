f00bce1f61de20403dd97c11d9d0fa632eadb4c9
Trần Anh Sơn <sontrananh96@gmail.com>
2017-11-24 17:28:10 +0700

    ADMIN

---
+++ JobStoreManage/app/src/main/java/com/example/sontr/jobstoremanage/ADMIN/activities/AdminProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.sontr.jobstoremanage.ADMIN.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.sontr.jobstoremanage.R;
+
+public class AdminProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_profile);
+    }
+}
---
+++ JobStoreManage/app/src/main/java/com/example/sontr/jobstoremanage/ADMIN/fragments/ManageInfoFragment.java
@@ -0,0 +1,19 @@
+package com.example.sontr.jobstoremanage.ADMIN.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sontr.jobstoremanage.R;
+
+public class ManageInfoFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_manage_info, container, false);
+
+        return rootView;
+    }
+}
